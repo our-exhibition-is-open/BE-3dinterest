9ad3d7e90cd2081a925bb663de4acfddbf6d42ff
Yugyeong Park <bonbonly7@naver.com>
2023-10-11 09:16:33 +0900

    Initial setting

---
+++ src/main/java/com/team2/_3dinterest/Application.java
@@ -0,0 +1,13 @@
+package com.team2._3dinterest;
+
+import lombok.Builder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
