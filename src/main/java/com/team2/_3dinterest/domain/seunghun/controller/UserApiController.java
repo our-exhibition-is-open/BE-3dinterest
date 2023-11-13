8cf145585aa0cd4f97978daede2aba0e7d71fb39
Oh <eric0h@naver.com>
2023-11-14 04:32:15 +0900

    /api/login 요청을 통해 로그인 API 구현

--- src/main/java/com/team2/_3dinterest/domain/seunghun/controller/UserApiController.java
+++ src/main/java/com/team2/_3dinterest/domain/seunghun/controller/UserApiController.java
@@ -8,6 +8,12 @@ import com.team2._3dinterest.domain.seunghun.user.UserCreateForm;
 import com.team2._3dinterest.domain.seunghun.user.SiteUser;
 import com.team2._3dinterest.domain.seunghun.service.UserService;
 import com.team2._3dinterest.domain.yugyeong.upload.dto.FileDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import jakarta.validation.Valid;
 
 import org.springframework.http.HttpStatus;
@@ -16,10 +22,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.beans.factory.annotation.Value;
 
 import lombok.RequiredArgsConstructor;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -31,6 +42,11 @@ public class UserApiController {
     private final UserService userService;
     private final UserRepository userRepository;
     private final FileDetailRepository fileDetailRepository;
+    private final AuthenticationManager authenticationManager;
+
+    @Value("${jwt.secret}") //application.yml에 설정된 시크릿 키 값을 가져옴
+    private String secretKey;
+
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@Valid @RequestBody UserCreateForm userCreateForm) {
@@ -72,12 +88,32 @@ public class UserApiController {
             return ResponseEntity.notFound().build();
         }
     }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Map<String, String> credentials) {
+        String username = credentials.get("username");
+        String password = credentials.get("password");
 
+        // Spring Security를 사용하여 인증 수행
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
 
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            // 성공적으로 인증이 완료되면 토큰을 발급하거나 세션 처리 등을 수행
+            String token = generateToken(username);
+
+            return ResponseEntity.ok(token);
+        } catch (Exception e) {
+            // 인증 실패 시 처리
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("인증 실패");
+        }
+    }
+    // JWT 토큰 생성
+    private String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
 }
-
-
-
-
-
-
