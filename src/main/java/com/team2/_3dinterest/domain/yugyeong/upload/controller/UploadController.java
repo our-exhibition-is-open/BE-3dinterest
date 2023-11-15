70520cdbc66ba2a7df76109d6955cfac199d1b06
Yugyeong Park <bonbonly7@naver.com>
2023-11-15 10:49:31 +0900

    RequestParam들을 PostFileDto로 받도록 변경

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/controller/UploadController.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/controller/UploadController.java
@@ -1,14 +1,13 @@
 package com.team2._3dinterest.domain.yugyeong.upload.controller;
 
-import com.team2._3dinterest.domain.yugyeong.upload.service.FileDetail;
+import com.team2._3dinterest.domain.yugyeong.upload.dto.FileDetail;
+import com.team2._3dinterest.domain.yugyeong.upload.dto.PostFileDto;
 import com.team2._3dinterest.domain.yugyeong.upload.service.FileUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/upload", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -18,18 +17,11 @@ public class UploadController {
 
     @PostMapping
     public ResponseEntity<FileDetail> post(
-            @RequestParam("title") String title,                            // 제목
-            @RequestParam("userName") String userName,                      // 업로드한 사람
-            @RequestParam("tagA") boolean tagA,                             // 태그A
-            @RequestParam("tagB") boolean tagB,                             // 태그B
-            @RequestParam("tagC") boolean tagC,                             // 태그C
-            @RequestParam("tagD") boolean tagD,                             // 태그D
-            @RequestPart("file") MultipartFile uploadImg,                   // 업로드 이미지
-            @RequestPart("file") List<MultipartFile> fileList) {   // 3D 파일
+            @RequestPart PostFileDto postFileDto) {
 
         try {
-            checkRequiredParameters(title, userName, tagA, tagB, tagC, tagD, uploadImg, fileList);
-            return ResponseEntity.ok(fileUploadService.save(title, userName, tagA, tagB, tagC, tagD, uploadImg, fileList)); // 성공, 200 OK 생성
+            checkRequiredParameters(postFileDto);
+            return ResponseEntity.ok(fileUploadService.save(postFileDto)); // 성공, 200 OK 생성
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); // 매개변수 부족, 400 BAD_REQUEST 생성
         } catch (Exception e) {
@@ -38,8 +30,11 @@ public class UploadController {
     }
 
     // 매개변수 체크
-    private void checkRequiredParameters(String title, String userName, boolean tagA, boolean tagB, boolean tagC, boolean tagD, MultipartFile uploadImg, List<MultipartFile> multipartFileList) throws IllegalArgumentException {
-        if (title == null || userName == null || uploadImg == null || multipartFileList == null) {
+    private void checkRequiredParameters(PostFileDto postFileDto) throws IllegalArgumentException {
+        if (postFileDto.getTitle() == null
+                || postFileDto.getUserName() == null
+                || postFileDto.getUploadImage() == null
+                || postFileDto.getFileList() == null) {
             throw new IllegalArgumentException("All required parameters must be provided.");
         }
     }
