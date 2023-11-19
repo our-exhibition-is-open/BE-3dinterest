d0944f87730089167b8631e8e7e431941af4549f
Yugyeong Park <bonbonly7@naver.com>
2023-11-20 01:01:57 +0900

    이미지와 파일 모두 s3에 업로드

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/service/FileUploadService.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/service/FileUploadService.java
@@ -13,21 +13,18 @@ import java.util.List;
 public class FileUploadService {
     private final AmazonS3ResourceStorage amazonS3ResourceStorage;
 
-    public List<ResponseFileDto> save(RequestUploadDto requestUploadDto) {
-        List<MultipartFile> fileList = requestUploadDto.getFileList();                      // RequestUploadDto의 File들을 List 형태로 저장
-        List<ResponseFileDto> responseFileDtoList = ResponseFileDto.multipartOf(fileList);  // File들에 대한 정보를 저장
-
-        MultipartFile uploadImage = requestUploadDto.getUploadImage();
-        ResponseFileDto responseImage = ResponseFileDto.ResponseImage(uploadImage);
-        responseFileDtoList.add(responseImage);                                             // 썸네일 업로드
-
-        // path 정보와 file을 s3에 저장
+    public List<ResponseFileDto> save(MultipartFile image, List<MultipartFile> fileList, RequestUploadDto requestUploadDto) {
+        List<ResponseFileDto> responseFileDtoList = ResponseFileDto.multipartOf(fileList); // File들에 대한 정보를 저장
         for (int i = 0; i < fileList.size(); i++) {
             MultipartFile file = fileList.get(i);
             ResponseFileDto responseFileDto = responseFileDtoList.get(i);
 
-            amazonS3ResourceStorage.store(responseFileDto.getPath(), file); // 파일, 썸네일 업로드
+            amazonS3ResourceStorage.store(responseFileDto.getPath(), file); // 파일 업로드
         }
+
+        ResponseFileDto responseImage = ResponseFileDto.ResponseImage(image); // File들에 대한 정보를 저장
+        responseFileDtoList.add(responseImage);
+        amazonS3ResourceStorage.store(responseImage.getPath(), image); // 썸네일 업로드
 
         return responseFileDtoList;
     }
