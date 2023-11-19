3929ca2d0676e4948ce642d5f7435e3440d830a1
Yugyeong Park <bonbonly7@naver.com>
2023-11-19 23:48:30 +0900

    업로드할 때 요청되는 파라미터를 받는 Dto이기 때문에, RequestFileDto의 이름을 직관적으로 RequestUploadDto로 변경

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/dto/RequestUploadDto.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/dto/RequestUploadDto.java
@@ -3,12 +3,14 @@ package com.team2._3dinterest.domain.yugyeong.upload.dto;
 import lombok.Builder;
 import lombok.Data;
 import javax.validation.constraints.NotNull;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @Data
+@Setter
 @Builder
-public class RequestFileDto {
+public class RequestUploadDto {
     @NotNull(message = "No title")
     private String title;
     @NotNull(message = "No userName")
