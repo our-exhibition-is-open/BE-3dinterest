6d2e55733541be7af95ad11ce824b6fdcda4c989
Yugyeong Park <bonbonly7@naver.com>
2023-11-19 23:49:44 +0900

    예외처리는 controller가 아닌 서비스에서 할 수 있도록 변경 예정

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/controller/UploadController.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/controller/UploadController.java
@@ -1,41 +1,25 @@
 package com.team2._3dinterest.domain.yugyeong.upload.controller;
 
-import com.team2._3dinterest.domain.yugyeong.upload.dto.FileDetail;
-import com.team2._3dinterest.domain.yugyeong.upload.dto.PostFileDto;
+import com.team2._3dinterest.domain.yugyeong.upload.dto.ResponseFileDto;
+import com.team2._3dinterest.domain.yugyeong.upload.dto.RequestUploadDto;
 import com.team2._3dinterest.domain.yugyeong.upload.service.FileUploadService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/upload", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/upload",
+        consumes = {MediaType.MULTIPART_FORM_DATA_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE} )
 @RequiredArgsConstructor
 public class UploadController {
     private final FileUploadService fileUploadService;
 
     @PostMapping
-    public ResponseEntity<FileDetail> post(
-            @RequestPart PostFileDto postFileDto) {
-
-        try {
-            checkRequiredParameters(postFileDto);
-            return ResponseEntity.ok(fileUploadService.save(postFileDto)); // 성공, 200 OK 생성
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); // 매개변수 부족, 400 BAD_REQUEST 생성
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null); // 서버 에러, 500 INTERNAL_SERVER_ERROR 생성
-        }
-    }
-
-    // 매개변수 체크
-    private void checkRequiredParameters(PostFileDto postFileDto) throws IllegalArgumentException {
-        if (postFileDto.getTitle() == null
-                || postFileDto.getUserName() == null
-                || postFileDto.getUploadImage() == null
-                || postFileDto.getFileList() == null) {
-            throw new IllegalArgumentException("All required parameters must be provided.");
-        }
+    public ResponseEntity<List<ResponseFileDto>> post(@Valid @RequestPart RequestUploadDto requestUploadDto) {
+         return ResponseEntity.ok(fileUploadService.save(requestUploadDto)); // 성공, 200 OK 생성
     }
 }
