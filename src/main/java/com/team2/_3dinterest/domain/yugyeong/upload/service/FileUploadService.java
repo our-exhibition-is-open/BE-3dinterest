000f85cd8ce5c34c8ef365a230223aac1dda6498
Yugyeong Park <bonbonly7@naver.com>
2023-11-19 23:44:08 +0900

    필요한 파라미터를 dto로 받고, 여러 개의 파일을 받을 수 있도록 변경

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/service/FileUploadService.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/service/FileUploadService.java
@@ -1,19 +1,34 @@
 package com.team2._3dinterest.domain.yugyeong.upload.service;
 
-import com.team2._3dinterest.domain.yugyeong.upload.repository.AmazonS3ResourceStorage;
-import com.team2._3dinterest.domain.yugyeong.upload.repository.FileDetail;
+import com.team2._3dinterest.domain.yugyeong.upload.AmazonS3ResourceStorage;
+import com.team2._3dinterest.domain.yugyeong.upload.dto.ResponseFileDto;
+import com.team2._3dinterest.domain.yugyeong.upload.dto.RequestUploadDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FileUploadService {
     private final AmazonS3ResourceStorage amazonS3ResourceStorage;
 
-    public FileDetail save(MultipartFile multipartFile) {
-        FileDetail fileDetail = FileDetail.multipartOf(multipartFile);
-        amazonS3ResourceStorage.store(fileDetail.getPath(), multipartFile);
-        return fileDetail;
+    public List<ResponseFileDto> save(RequestUploadDto requestUploadDto) {
+        List<MultipartFile> fileList = requestUploadDto.getFileList();                      // RequestUploadDto의 File들을 List 형태로 저장
+        List<ResponseFileDto> responseFileDtoList = ResponseFileDto.multipartOf(fileList);  // File들에 대한 정보를 저장
+
+        MultipartFile uploadImage = requestUploadDto.getUploadImage();
+        ResponseFileDto responseImage = ResponseFileDto.ResponseImage(uploadImage);
+        responseFileDtoList.add(responseImage);                                             // 썸네일 업로드
+
+        // path 정보와 file을 s3에 저장
+        for (int i = 0; i < fileList.size(); i++) {
+            MultipartFile file = fileList.get(i);
+            ResponseFileDto responseFileDto = responseFileDtoList.get(i);
+
+            amazonS3ResourceStorage.store(responseFileDto.getPath(), file); // 파일, 썸네일 업로드
+        }
+
+        return responseFileDtoList;
     }
 }
