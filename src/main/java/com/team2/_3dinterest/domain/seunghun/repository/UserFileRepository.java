b050f43585ceec0fc3c809e6df48eb8c539ca0f7
Seunghun Oh <eric0h@naver.com>
2023-11-21 16:04:22 +0900

    /files/byParentID 입력시 여러 postID 중에서 제공된 parentID를 가지는 모든 postID를 불러오고 불러온 postID들을 조회해서 각 postID들이 가지는 모든 파일 상세 정보를 배열의 형태로 전달
    이 모든 파일 상세 정보를 모아다가 배열 형태로 반환

--- src/main/java/com/team2/_3dinterest/domain/seunghun/repository/UserFileRepository.java
+++ src/main/java/com/team2/_3dinterest/domain/seunghun/repository/UserFileRepository.java
@@ -3,9 +3,14 @@ package com.team2._3dinterest.domain.seunghun.repository;
 import com.team2._3dinterest.domain.seunghun.user.UserFile;
 import com.team2._3dinterest.domain.seunghun.user.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UserFileRepository extends JpaRepository<UserFile, Long> {
     List<UserFile> findByUser(SiteUser user);
+    List<UserFile> findByPostID(String postID);
+    @Query("SELECT DISTINCT uf.postID FROM UserFile uf WHERE uf.parentID = :parentID")
+    List<String> findPostIDsByParentID(@Param("parentID") String parentID);
 }
