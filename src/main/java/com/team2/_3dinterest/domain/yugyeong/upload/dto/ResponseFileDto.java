ea6f9dc3b6de73df185a8e7fdd338c600101055b
Yugyeong Park <bonbonly7@naver.com>
2023-11-21 23:49:45 +0900

    path 필드를 model_path와 image_path로 분할

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/dto/ResponseFileDto.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/dto/ResponseFileDto.java
@@ -21,7 +21,8 @@ public class ResponseFileDto {
     private String id;      // 36자리의 UUID
     private String name;    // 파일 업로드 시점의 파일명
     private String format;  // 파일 확장자
-    private String path;    // 파일의 실제 물리적 경로값
+    private String model_path;
+    private String image_path;    // 파일의 실제 물리적 경로값
     private long bytes;     // 바이트
 
     @Builder.Default
@@ -39,7 +40,7 @@ public class ResponseFileDto {
                     .id(fileId)
                     .name(file.getOriginalFilename())
                     .format(format)
-                    .path(MultipartUtil.createPath(fileId, format))
+                    .model_path(MultipartUtil.createPath(fileId, format))
                     .bytes(file.getSize())
                     .build();
 
@@ -48,6 +49,7 @@ public class ResponseFileDto {
         return responseFileDtoList;
     }
 
+    // image
     public static ResponseFileDto ResponseImage(MultipartFile image) {
         final String fileId = MultipartUtil.createUUID();
         final String format = MultipartUtil.getFormat(image.getContentType());
@@ -56,7 +58,7 @@ public class ResponseFileDto {
                 .id(fileId)
                 .name(image.getOriginalFilename())
                 .format(format)
-                .path(MultipartUtil.createPath(fileId, format))
+                .image_path(MultipartUtil.createPath(fileId, format))
                 .bytes(image.getSize())
                 .build();
     }
