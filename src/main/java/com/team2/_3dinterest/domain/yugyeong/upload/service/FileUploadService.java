53c25e31d58e79d24196244de792db5f1563b81f
Yugyeong Park <bonbonly7@naver.com>
2023-11-24 16:57:40 +0900

    Dto를 Entity로 변환 후 Repository에 저장한다.

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/service/FileUploadService.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/upload/service/FileUploadService.java
@@ -3,6 +3,8 @@ package com.team2._3dinterest.domain.yugyeong.upload.service;
 import com.team2._3dinterest.domain.yugyeong.upload.AmazonS3ResourceStorage;
 import com.team2._3dinterest.domain.yugyeong.upload.dto.ResponseFileDto;
 import com.team2._3dinterest.domain.yugyeong.upload.dto.RequestUploadDto;
+import com.team2._3dinterest.domain.yugyeong.upload.entity.UploadEntity;
+import com.team2._3dinterest.domain.yugyeong.upload.repository.UploadRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FileUploadService {
     private final AmazonS3ResourceStorage amazonS3ResourceStorage;
+    private final UploadRepository uploadRepository;
 
     @Transactional
     public List<ResponseFileDto> save(MultipartFile image, List<MultipartFile> fileList, RequestUploadDto requestUploadDto) {
@@ -24,12 +27,21 @@ public class FileUploadService {
             ResponseFileDto responseFileDto = responseFileDtoList.get(i);
 
             amazonS3ResourceStorage.store(responseFileDto.getModel_path(), file); // model 업로드
+
+            // Dto를 UploadEntity로 변환
+            UploadEntity uploadEntity = UploadEntity.toEntity(requestUploadDto, responseFileDto);
+            uploadRepository.save(uploadEntity);
         }
 
         // image
         ResponseFileDto responseImage = ResponseFileDto.ResponseImage(image); // File들에 대한 정보를 저장
+
         responseFileDtoList.add(responseImage);
         amazonS3ResourceStorage.store(responseImage.getImage_path(), image); // image 업로드
+
+        // Dto를 UploadEntity로 변환
+        UploadEntity uploadEntity = UploadEntity.toEntity(requestUploadDto, responseImage);
+        uploadRepository.save(uploadEntity);
 
         return responseFileDtoList;
     }
