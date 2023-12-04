5c4e15af67c682ee18c581f225b626cbb97c1ba7
Oh <eric0h@naver.com>
2023-12-05 04:47:13 +0900

    UserSecurityService에서 getUsername수정

--- src/main/java/com/team2/_3dinterest/domain/seunghun/service/UserSecurityService.java
+++ src/main/java/com/team2/_3dinterest/domain/seunghun/service/UserSecurityService.java
@@ -24,7 +24,7 @@ public class UserSecurityService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<SiteUser> _siteUser = this.userRepository.findByUsername(username);
+        Optional<SiteUser> _siteUser = this.userRepository.findByUserName(username);
         if (_siteUser.isEmpty()) {
             throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
         }
