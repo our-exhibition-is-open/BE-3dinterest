4ee908a2b3828115bdc6f2c06aa85b3aafdede5f
kimahhyeon <kah0220@naver.com>
2023-12-05 13:37:44 +0900

    게시글 클래스 정의

---
+++ src/main/java/com/team2/_3dinterest/domain/ahyeon/post/Post.java
@@ -0,0 +1,12 @@
+package com.team2._3dinterest.domain.ahyeon.post;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Post {
+    private Long postId;
+    private int likes;
+}
