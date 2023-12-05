4085e376bbdda1ba862bbfee1b7e1e1bf414d123
Yugyeong Park <bonbonly7@naver.com>
2023-12-05 19:33:47 +0900

    게시물 조회를 위한 Controller, PostEntity를 List로 return한다.

---
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/postlist/controller/PostListController.java
@@ -0,0 +1,21 @@
+package com.team2._3dinterest.domain.yugyeong.postlist.controller;
+
+import com.team2._3dinterest.domain.yugyeong.entity.PostEntity;
+import com.team2._3dinterest.domain.yugyeong.postlist.service.PostListService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping( value = "/postlist" )
+@RequiredArgsConstructor
+public class PostListController {
+    private final PostListService postListService;
+
+    @GetMapping
+    public ResponseEntity<List<PostEntity>> postlist() throws IOException {
+        return postListService.postlist(); // 성공, 200 OK 생성
+    }
+}
