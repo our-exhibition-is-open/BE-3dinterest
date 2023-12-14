72500981cfd399557da9d5d59e29d4dc4f5002f1
Yugyeong Park <bonbonly7@naver.com>
2023-12-15 01:09:29 +0900

    사용하지 않는 IOExeption 삭제

--- src/main/java/com/team2/_3dinterest/domain/yugyeong/postlist/controller/PostListController.java
+++ src/main/java/com/team2/_3dinterest/domain/yugyeong/postlist/controller/PostListController.java
@@ -5,7 +5,6 @@ import com.team2._3dinterest.domain.yugyeong.postlist.service.PostListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -15,7 +14,7 @@ public class PostListController {
     private final PostListService postListService;
 
     @GetMapping
-    public ResponseEntity<List<PostEntity>> postlist() throws IOException {
+    public ResponseEntity<List<PostEntity>> postlist() {
         return postListService.postlist(); // 성공, 200 OK 생성
     }
 }
